ba526c99f3a008bd9d42aab856c1fcf8366e8f10
Speeder421 <davidjesselim4@gmail.com>
2025-10-26 03:39:45 +0000

    hi

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,22 @@
+let bird;
+let background, birdImg;
 
+function preload(){
+    background=loadImage("assets/background.png");
+    birdImg=loadImage("assets/bird.png");
+}
+
+function setup(){
+    new Canvas(400, 600);
+
+    bird=new Sprite();
+    bird.x=width/2;
+    bird.y=height/2;
+    bird.width=34;
+    bird.height=24;
+    bird.img=birdImg;
+   
+}
+function draw(){
+    image(background, 0, 0, width, height);
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,20 @@
+let square;
 
+function setup(){
+    new Canvas(400, 600);
+   
+    // code to set the gravity of the world
+    world.gravity.y=10;
+    // code to create a new square sprite
+    square= new Sprite(width/2,height/2,40,40);
+}
+
+function draw(){
+    background(225);
+    if(mouse.presses()){
+        square.vel.y=-5;
+        square.sleeping=false;
+    }
+    // code to check if mouse is pressed i.e. mouse.presses()
+        // if mouse is pressed, the square sprite "flies" up
+}
